5231ad8ed478761e8634c5e4fb58399888f92b40
sshafeez <sshafeez@umich.edu>
2018-12-27 17:48:00 -0500

    first 3 excercises

---
+++ machine-learning-ex2/machine-learning-ex2/ex2/plotData.m
@@ -0,0 +1,32 @@
+function plotData(X, y)
+%PLOTDATA Plots the data points X and y into a new figure 
+%   PLOTDATA(x,y) plots the data points with + for the positive examples
+%   and o for the negative examples. X is assumed to be a Mx2 matrix.
+
+% Create New Figure
+figure; hold on;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Plot the positive and negative examples on a
+%               2D plot, using the option 'k+' for the positive
+%               examples and 'ko' for the negative examples.
+%
+admitted = find(y==1);
+rejected = find(y==0);
+plot(X(admitted,1),X(admitted,2),'go')
+plot(X(rejected,1),X(rejected,2),'rx')
+
+
+
+
+
+
+
+
+% =========================================================================
+
+
+
+hold off;
+
+end
---
+++ machine-learning-ex3/machine-learning-ex3/ex3/predict.m
@@ -0,0 +1,41 @@
+function p = predict(Theta1, Theta2, X)
+%PREDICT Predict the label of an input given a trained neural network
+%   p = PREDICT(Theta1, Theta2, X) outputs the predicted label of X given the
+%   trained weights of a neural network (Theta1, Theta2)
+
+% Useful values
+m = size(X, 1);
+num_labels = size(Theta2, 1);
+
+X = [ones(m, 1) X];
+
+% You need to return the following variables correctly 
+p = zeros(size(X, 1), 1);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Complete the following code to make predictions using
+%               your learned neural network. You should set p to a 
+%               vector containing labels between 1 to num_labels.
+%
+% Hint: The max function might come in useful. In particular, the max
+%       function can also return the index of the max element, for more
+%       information see 'help max'. If your examples are in rows, then, you
+%       can use max(A, [], 2) to obtain the max for each row.
+%
+for i=1:m
+    z_two = Theta1*X(i,:)';
+    a_two = sigmoid(z_two);
+    a_two = [1; a_two];
+    
+    z_three = Theta2*a_two;
+    a_three = sigmoid(z_three);
+    [~,p(i)] = max(a_three,[],1);
+end
+
+
+
+
+% =========================================================================
+
+
+end
